49a813415cf0f19ded9b274372767368acebe7d1
ceresgalax <ceresgalax@gmail.com>
2024-02-19 22:08:09 -0800

    Add uint implicit conversion to uint, operator++ support.

---
+++ ImguiNodeEditor.NET/LinkId.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AxNodeEditorNET
+{
+    public partial struct LinkId
+    {        
+        public static unsafe implicit operator LinkId(uint index)
+        {
+            return new LinkId() {
+                _base_Details_SafePointerType_LinkId = new Details_SafePointerType_LinkId() {
+                    _base_Details_SafeType_voidptr_LinkId = new Details_SafeType_voidptr_LinkId() {
+                        m_Value = (void*)index
+                    }
+                }
+            };
+        }
+
+        public static unsafe LinkId operator ++(in LinkId id)
+        {
+            IntPtr ptr = (IntPtr)id._base_Details_SafePointerType_LinkId._base_Details_SafeType_voidptr_LinkId.m_Value;
+            return new LinkId() {
+                _base_Details_SafePointerType_LinkId = new Details_SafePointerType_LinkId() {
+                    _base_Details_SafeType_voidptr_LinkId = new Details_SafeType_voidptr_LinkId() {
+                        m_Value = (void*)(ptr + 1)
+                    }
+                }
+            };
+        }
+        
+    }
+}
