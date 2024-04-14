1b0e68ad99642970d314627105d1dbbc1d6b8a3f
ceresgalax <ceresgalax@gmail.com>
2024-04-14 15:29:31 -0700

    Add 'Editor class for managing editor contexts.

---
+++ ImguiNodeEditor.NET/Editor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AxNodeEditorNET
+{
+    public sealed class Editor : IDisposable
+    {
+        public readonly IntPtr Context;
+
+        public Editor(ref Config config)
+        {
+            Context = AxNodeEditor.CreateEditor(ref config);
+        }
+        
+        private void ReleaseUnmanagedResources()
+        {
+            AxNodeEditor.DestroyEditor(Context);
+        }
+
+        public void Dispose()
+        {
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+
+        ~Editor()
+        {
+            ReleaseUnmanagedResources();
+        }
+
+    }
+}
